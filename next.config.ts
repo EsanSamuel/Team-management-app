861dd5db225790606eb6347aa1c769f40a24bc1a
EsanSamuel <esansamuel555@gmail.com>
2025-06-27 01:41:59 +0100

    set up rhel-openssl-3.0.x

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
-    domains: ["lh3.googleusercontent.com"], 
+    domains: ["lh3.googleusercontent.com"],
   },
 };
 
