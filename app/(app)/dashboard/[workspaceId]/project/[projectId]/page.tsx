5f1661f75abd20d8980788b1f5ea1b0189fdb921
Esan Samuel <esansamuel555@gmail.com>
2025-08-04 19:37:47 +0100

    Update page.tsx

--- app/(app)/dashboard/[workspaceId]/project/[projectId]/page.tsx
+++ app/(app)/dashboard/[workspaceId]/project/[projectId]/page.tsx
@@ -386,7 +386,8 @@ const Page = () => {
                 />
               )}
 
-              {view === "Karban" && <Karban tasks={tasks as any} view={view}/>}
+              {view === "Karban" && <Karban tasks={tasks as any} view={view}
+ setView={setView}/>}
             </>
           ) : (
             <div className="flex items-center justify-center h-full mt-20  ">
